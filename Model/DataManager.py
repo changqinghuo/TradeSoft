354bbd35ddb70ffa8d0203f5253fa2531a12efbf
terry <aa@aa.com>
2013-01-23 23:29:18 +0800

    add QuoteDataThread in DataManager.py

--- Model/DataManager.py
+++ Model/DataManager.py
@@ -1,22 +1,73 @@
 from quote import *
 from wx.lib.pubsub import Publisher as pub
+import datetime
+import threading
+import time
+
+class QuoteDataThread(threading.Thread):
+    def __init__(self, symbol, period=300, num_day=5):
+        threading.Thread.__init__(self)
+        self.symbol = symbol
+        self.period = period
+        self.num_day = num_day
+        self.dt = datetime.datetime(2000, 1, 1)
+        self.quotedata = None
+        self.lastupdate_time = datetime.datetime(2000, 1, 1)
+        
+    def run(self):
+        done = False
+        while not done:
+            try:
+                q = GoogleIntradayQuote(self.symbol,self.period, self.num_day)
+                if q.datetime[-1] > self.dt:
+                    self.lastupdate_time = q.datetime[-1]
+                    self.quotedata = q 
+                
+                done = True           
+                
+            except:
+                done = False
 
 class DataManager(object):
     """Singleton datamanager"""
     def __init__(self):
-        self.quotedata = None        
-        pass
+        self.quotedata = None
+        self.symbol_quote_dict = {"002094":None, '000001':None} 
+        
+                  
+
     def GetQuoteData(self, quote, period=300, num_day=5): 
         try:       
             self.quotedata = GoogleIntradayQuote(quote,period, num_day)
             pub.sendMessage("ANALYSISDATA", self.quotedata.df)
             return self.quotedata
         except:
-            pass
-        
+            pass       
         
     def GetQuoteRealtime(self, quote):
         pass
     
-    
+    def QuoteDataThreads(self):
+        thread_list = []
+        for d in self.symbol_quote_dict.keys(): 
+            t = QuoteDataThread(d)
+            t.start()            
+            thread_list.append(t)
+        done = True
+        while True:
+            done = True
+            for t in thread_list: 
+                time.sleep(5)               
+                if t.is_alive():
+                    done = False
+                else:
+                    self.symbol_quote_dict[t.symbol] = t
+                    #if t.lastupdate_time > self.symbol_quote_dict.setdefault(t.symbol, None).lastupdate_time:
+                    pub.sendMessage(t.symbol+"ANALYSISDATA", t.quotedata.df)
+                       
+        
+            
+        
+        
+        
 
--- control/control.py
+++ control/control.py
@@ -4,9 +4,10 @@ from view.AnalysisPanel import *
 from view.MainWindow import *
 from model.DataManager import *
 from util.draw import *
+import threading
 
 class Control:
-    def __init__(self, app):
+    def __init__(self, app):        
         self.model = DataManager() 
         self.mainwindow = MyParentFrame()           
         self.mainwindow.Bind(wx.EVT_MENU, self.OnNewAnalysisWindow, id=ID_Menu_New)
@@ -18,32 +19,37 @@ class Control:
         app.SetTopWindow(self.mainwindow)
         self.analysis_data = self.model.GetQuoteData('002094', 300, 5).df
         
-        pub.subscribe(self.AnalysisDataArrive, "ANALYSISDATA")
+        for d in self.model.symbol_quote_dict.keys():            
+            pub.subscribe(self.AnalysisDataArrive, d+"ANALYSISDATA")
+        
 
 
     def OnAnalysisPaint(self, evt):
        dc = wx.PaintDC(self.analysis_panel)
-       #dc.DrawBitmap(self.Buffer, 0, 0)      
+       #dc.DrawBitmap(self.Buffer, 0, 0) 
+       dc.Clear()     
        draw_candle(dc, self.analysis_data)
                 
 
     def OnEraseBack(self, evt):        
         pass
-    def AnalysisDataArrive(self, message):
+    def AnalysisDataArrive(self, message):        
         self.analysis_data = message.data
-        self.analysis_panel.refresh()
+        self.analysis_panel.Refresh()
     def OnTimer(self, evt):
         self.model.GetQuoteData('002094', 300, 5)
     def OnNewAnalysisWindow(self, evt):
         win = wx.MDIChildFrame(self.mainwindow, -1, "Child Window: %d" % 1)
         self.timer = wx.Timer(win)
-        win.Bind(wx.EVT_TIMER, self.OnTimer, self.timer)
+        #win.Bind(wx.EVT_TIMER, self.OnTimer, self.timer)
         self.analysis_panel = AnalysisPanel(win)
         self.analysis_panel.Bind(wx.EVT_PAINT, self.OnAnalysisPaint)
         self.analysis_panel.Bind(wx.EVT_ERASE_BACKGROUND, self.OnEraseBack)
-        self.timer.Start(5000)        
+        #self.timer.Start(5000)        
         #canvas = ScrolledWindow.MyCanvas(win)
         win.Show(True)
+        t = threading.Thread(target=self.model.QuoteDataThreads())
+        t.start()
 
         
         
