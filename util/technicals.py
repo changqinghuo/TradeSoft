34615c9ea8a4cc23249b4dfd2856423385e54941
unknown <211031@CNSHA02L00054VS.dom4.ad.sys>
2013-02-20 18:04:57 +0800

    Add bi function

--- util/technicals.py
+++ util/technicals.py
@@ -2,6 +2,12 @@ import wx
 import pandas as pd
 from model.quote import Quote
 from util.draw import draw_candle
+
+DIRECTION_UP = 0
+DIRECTION_DOWN = 1
+K_DING = -1
+K_DI = 1
+
 def macd(df):
     pass
 
@@ -14,7 +20,97 @@ def _process_contain(df):
     
     pass
 def _bi(df):
-    pass
+    highlast = df.ix[0]["high"]
+    lowlast = df.ix[0]["low"]
+    for i in range(1, len(df.index)):
+        high = df.ix[i]["high"]
+        low = df.ix[i]["low"]
+        if high > highlast and low > lowlast:
+            direction = DIRECTION_UP
+            break
+        elif low < lowlast and high < highlast:
+            direction = DIRECTION_DOWN
+            break
+           
+    knum_up = 0
+    knum_down = 0
+
+    result = []
+    result.append(0)
+    i = 1
+    while i < len(df):
+        high = df.ix[i]["high"]
+        low = df.ix[i]["low"]
+        i = i+1       
+       
+       #up trend
+        while i < len(df):           
+            
+            high = df.ix[i]["high"]
+            low = df.ix[i]["low"]        
+            i = i + 1
+            if high > highlast:
+                knum_up = knum_up +1
+                highlast = high
+                lowlast = low
+            elif high <= highlast and low >= lowlast:
+                lowlast = low
+            else:
+                break
+        if knum_up >= 5 and direction == DIRECTION_DOWN:
+            direction = DIRECTION_UP
+            result.append(i)
+        knum_up = 0
+        
+        #down trend
+        while i < len(df):         
+            high = df.ix[i]["high"]
+            low = df.ix[i]["low"] 
+            i = i +1 
+            if low < lowlast:                
+                knum_down = knum_down + 1
+                highlast = high
+                lowlast = low
+            elif high <= highlast and low >= lowlast:
+                highlast = high
+            else:
+                break
+        if knum_down >= 5:
+            direction = DIRECTION_DOWN
+            result.append(i)
+        knum_down = 0 
+        
+    return result
+         
+            
+        
+#    for i in range(1, len(df.index)):
+#        high = df.ix[i]["high"]
+#        low = df.ix[i]["low"]
+#        #contain
+#        if high <= highlast and low >= lowlast:
+#            if direction == DIRECTION_UP:
+#                lowlast = low
+#            else:
+#                highlast = high            
+#        elif high < highlast and low < lowlast and direction == DIRECTION_DOWN:
+#            highlast = high
+#            lowlast = low
+#            knum_olddir = knum_olddir + 1
+#        elif high > highlast and low > lowlast and direction == DIRECTION_UP: 
+#            highlast = high
+#            lowlast = low
+#            knum_olddir = knum_olddir + 1
+        
+        
+        
+            
+            
+        
+        
+        
+    
+
 def _duan(df):
     pass
 def _zhongshu(df):
@@ -37,6 +133,7 @@ def _thirdsell(df):
 def test():
     "test case for czsc technical"
     df = pd.DataFrame.from_csv('600016test.csv')
+    print _bi(df)
     class TestPanel(wx.Panel): 
         def __init__(self, parent):
             wx.Panel.__init__(self, parent)
